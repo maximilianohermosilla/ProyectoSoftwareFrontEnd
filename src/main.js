555b0848f0afecac3f8fc387e69211abab1caa72
RYZEN-5600X\maxim <maximiliano_hermosilla@hotmail.com>
2023-05-13 03:24:13 -0300

    Restyling navbar

--- src/main.js
+++ src/main.js
@@ -2,27 +2,46 @@ import apiMercaderias from '/src/services/apiMercaderias.js'
 import RenderCard from '/src/views/cardMercaderia.js'
 import RenderDetalle from '/src/views/detalleMercaderia.js'
 
-let modalHtml = '/pages/modalMercaderiaDetalle.html';
+//Variables
 let listaMercaderias = [];
 let mercaderia;
 
-loadHTML(modalHtml);
-
-const getMercaderias = async () => {
-    listaMercaderias = await apiMercaderias.Get();
-    renderCards();
+//Consts
+const getMercaderias = async () => {  
+    let tipo= '';
+    let nombreElement = document.getElementById("txtSearch");
+    let nombre = nombreElement.value;
+    let orden = 'ASC';
+    listaMercaderias = await apiMercaderias.Get(tipo, nombre, orden);
+    await renderCards();
 }
 
 const getMercaderiaById = async (id) => {  
     mercaderia = await apiMercaderias.GetById(id);
-    console.log(mercaderia);
     renderDetalle(mercaderia);
 }
 
-getMercaderias();
+const onClickElement = (id) => {
+    getMercaderiaById(id);
+}
 
-function renderCards(){
+const searchButton = document.getElementById("btnSearch");
+searchButton.addEventListener('click', () =>{
+    console.log("click")
+    getMercaderias();      
+})
+
+//Load const
+setTimeout(() => {
+    getMercaderias();
+    
+}, 0);
+
+
+//Functions
+async function renderCards(){
     let cardsContainer = document.getElementById("cardContainer");
+    cardsContainer.innerHTML = '';
     listaMercaderias.forEach(mercaderia =>{ 
         cardsContainer.innerHTML += RenderCard(mercaderia);
     })    
@@ -36,15 +55,6 @@ function renderDetalle(mercaderia){
     detalleMercaderia.innerHTML = RenderDetalle(mercaderia);
 }
 
-const searchButton = document.getElementById("btnSearch");
-searchButton.addEventListener('click', () =>{
-    getMercaderias();      
-})
-
-const onClickElement = (id) => {
-    getMercaderiaById(id);
-}
-
 function onListItemClick(elements){
     elements.forEach((element) => {
         element.addEventListener('click', () =>{
@@ -53,10 +63,5 @@ function onListItemClick(elements){
     });
 }
 
-function loadHTML(html){
-    fetch(html)
-    .then(response=> response.text())
-    .then(text=> document.getElementById('cardContainer').innerHTML = text);
-}
 
 
