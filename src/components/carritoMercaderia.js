ee13fb13903294039bd4eb7be9c5007fd5ccb89f
maximiliano_hermosilla@hotmail.com <maximiliano_hermosilla@hotmail.com>
2023-07-06 18:54:41 -0300

    Fix css responsive

--- src/components/carritoMercaderia.js
+++ src/components/carritoMercaderia.js
@@ -1,10 +1,10 @@
 export default function RenderCarrito(mercaderia){
     return `
     <div class="row carrito">
-        <div class="carrito-img col-2 flex-center">
+        <div class="carrito-img col-3 flex-center">
             <img cross-origin="use-credentials" id="${mercaderia.id}" src="${mercaderia.imagen}" class="img-carrito" onerror="this.src='../images/notfound.png'">
         </div>
-        <div class="carrito-body col-lg-5 col-4 flex-center">
+        <div class="carrito-body col-lg-4 col-4 flex-center">
             <div class="div-body">
                 <h5 class="card-title">${mercaderia.nombre}
                 <h6 class="card-description green">${mercaderia.descripcion}</h6>
@@ -17,7 +17,7 @@ export default function RenderCarrito(mercaderia){
                 <span id="${mercaderia.id}" class="span-cantidad btn btn-outline-success btnAgregarCantidad">+</span>            
             </div>
         </div>
-        <div class="carrito-precio col-lg-2 col-3 flex-center">
+        <div class="carrito-precio col-lg-2 col-2 flex-center">
             <div class="span-precio">$${mercaderia.precio * mercaderia.cantidad}</div>
         </div>
         <div class="carrito-close col-1">
